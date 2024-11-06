7c8252ef93c99d717f554ccbb016bb0ebefd26a2
Brix John Novesteras <brixjohnovesteras@gmail.com>
2024-11-06 11:18:17 +0800

    landing image

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -9,7 +9,25 @@ const Landing = ({ setSelectedPage }) => {
     <section
       id="home"
       className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
-    ></section>
+    >
+      {/* IMAGE SECTION */}
+      <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
+        {isAboveMediumScreens ? (
+          <div
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+          >
+            <img
+              alt="profile"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              src="assets/profile-image.png"
+            />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </section>
   );
 };
 
