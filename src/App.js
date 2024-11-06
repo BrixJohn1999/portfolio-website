45849c3570db04f383b361c49547e8a02ff21e32
Brix John Novesteras <brixjohnovesteras@gmail.com>
2024-11-07 00:01:41 +0800

    add skills, line gradient

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Navbar from "./scenes/Navbar";
 import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
+import MySkills from "./scenes/MySkills";
+import Projects from "./scenes/Projects";
 import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
+import LineGradient from "./components/LineGradient";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -33,6 +36,13 @@ function App() {
           />
         )}
         <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <MySkills />
+      </div>
+      <div className="w-5/6 mx-auto">
+        <Projects />
       </div>
     </div>
   );
